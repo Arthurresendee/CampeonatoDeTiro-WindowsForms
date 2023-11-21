b97c62c11d20378d4cd8534143c89a9569ad0bb5
ArthurWork <Arthurresende2004@gmail.com>
2023-11-21 14:45:02 -0300

    Criado parte visual do formulário de login.

---
+++ ScreenLogin/Form_Login.Designer.cs
@@ -0,0 +1,208 @@
+﻿namespace ScreenLogin
+{
+    partial class Form_Login
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            panel1 = new Panel();
+            linklbl_recuperaSenha = new LinkLabel();
+            btn_Login = new Button();
+            txt_usuario = new TextBox();
+            label1 = new Label();
+            txt_senha = new TextBox();
+            lbl_senha = new Label();
+            lbl_usuario = new Label();
+            label5 = new Label();
+            label4 = new Label();
+            linklbl_cadastro = new LinkLabel();
+            panel1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // panel1
+            // 
+            panel1.BorderStyle = BorderStyle.Fixed3D;
+            panel1.Controls.Add(linklbl_cadastro);
+            panel1.Controls.Add(linklbl_recuperaSenha);
+            panel1.Controls.Add(btn_Login);
+            panel1.Controls.Add(txt_usuario);
+            panel1.Controls.Add(label1);
+            panel1.Controls.Add(txt_senha);
+            panel1.Controls.Add(lbl_senha);
+            panel1.Controls.Add(lbl_usuario);
+            panel1.Location = new Point(435, 49);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(281, 357);
+            panel1.TabIndex = 11;
+            // 
+            // linklbl_recuperaSenha
+            // 
+            linklbl_recuperaSenha.ActiveLinkColor = SystemColors.MenuHighlight;
+            linklbl_recuperaSenha.AutoSize = true;
+            linklbl_recuperaSenha.Font = new Font("Segoe UI", 7F, FontStyle.Regular, GraphicsUnit.Point);
+            linklbl_recuperaSenha.LinkColor = Color.White;
+            linklbl_recuperaSenha.Location = new Point(145, 225);
+            linklbl_recuperaSenha.Name = "linklbl_recuperaSenha";
+            linklbl_recuperaSenha.Size = new Size(98, 12);
+            linklbl_recuperaSenha.TabIndex = 8;
+            linklbl_recuperaSenha.TabStop = true;
+            linklbl_recuperaSenha.Text = "Esqueceu sua senha?";
+            // 
+            // btn_Login
+            // 
+            btn_Login.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            btn_Login.ForeColor = SystemColors.ActiveCaptionText;
+            btn_Login.Location = new Point(87, 266);
+            btn_Login.Name = "btn_Login";
+            btn_Login.Size = new Size(110, 33);
+            btn_Login.TabIndex = 7;
+            btn_Login.Text = "Login";
+            btn_Login.UseVisualStyleBackColor = true;
+            btn_Login.Click += btn_Login_Click;
+            // 
+            // txt_usuario
+            // 
+            txt_usuario.Location = new Point(37, 129);
+            txt_usuario.Name = "txt_usuario";
+            txt_usuario.Size = new Size(206, 23);
+            txt_usuario.TabIndex = 5;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.BackColor = SystemColors.ControlText;
+            label1.Font = new Font("Segoe UI", 25F, FontStyle.Bold, GraphicsUnit.Point);
+            label1.ForeColor = Color.FloralWhite;
+            label1.Location = new Point(87, 26);
+            label1.Name = "label1";
+            label1.Size = new Size(110, 46);
+            label1.TabIndex = 0;
+            label1.Text = "Login";
+            // 
+            // txt_senha
+            // 
+            txt_senha.Location = new Point(37, 199);
+            txt_senha.Name = "txt_senha";
+            txt_senha.Size = new Size(206, 23);
+            txt_senha.TabIndex = 6;
+            txt_senha.UseSystemPasswordChar = true;
+            // 
+            // lbl_senha
+            // 
+            lbl_senha.AutoSize = true;
+            lbl_senha.BackColor = SystemColors.ControlText;
+            lbl_senha.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            lbl_senha.ForeColor = Color.FloralWhite;
+            lbl_senha.Location = new Point(37, 175);
+            lbl_senha.Name = "lbl_senha";
+            lbl_senha.Size = new Size(53, 21);
+            lbl_senha.TabIndex = 2;
+            lbl_senha.Text = "Senha";
+            // 
+            // lbl_usuario
+            // 
+            lbl_usuario.AutoSize = true;
+            lbl_usuario.BackColor = SystemColors.ControlText;
+            lbl_usuario.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            lbl_usuario.ForeColor = Color.FloralWhite;
+            lbl_usuario.Location = new Point(37, 105);
+            lbl_usuario.Name = "lbl_usuario";
+            lbl_usuario.Size = new Size(64, 21);
+            lbl_usuario.TabIndex = 1;
+            lbl_usuario.Text = "Usuário";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.BackColor = SystemColors.ControlText;
+            label5.Font = new Font("Segoe UI", 25F, FontStyle.Bold, GraphicsUnit.Point);
+            label5.ForeColor = Color.FloralWhite;
+            label5.Location = new Point(135, 200);
+            label5.Margin = new Padding(0);
+            label5.Name = "label5";
+            label5.Size = new Size(244, 46);
+            label5.TabIndex = 10;
+            label5.Text = "shooting Club";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.BackColor = SystemColors.ControlText;
+            label4.Font = new Font("Segoe UI", 25F, FontStyle.Bold, GraphicsUnit.Point);
+            label4.ForeColor = Color.FloralWhite;
+            label4.Location = new Point(77, 162);
+            label4.Margin = new Padding(0);
+            label4.Name = "label4";
+            label4.Size = new Size(210, 46);
+            label4.TabIndex = 9;
+            label4.Text = "Welcome to";
+            // 
+            // linklbl_cadastro
+            // 
+            linklbl_cadastro.ActiveLinkColor = SystemColors.MenuHighlight;
+            linklbl_cadastro.AutoSize = true;
+            linklbl_cadastro.Font = new Font("Segoe UI", 7F, FontStyle.Regular, GraphicsUnit.Point);
+            linklbl_cadastro.LinkColor = Color.White;
+            linklbl_cadastro.Location = new Point(92, 314);
+            linklbl_cadastro.Name = "linklbl_cadastro";
+            linklbl_cadastro.Size = new Size(99, 12);
+            linklbl_cadastro.TabIndex = 9;
+            linklbl_cadastro.TabStop = true;
+            linklbl_cadastro.Text = "Não Possui Cadastro?";
+            // 
+            // Form_Login
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.ActiveCaptionText;
+            ClientSize = new Size(800, 450);
+            Controls.Add(panel1);
+            Controls.Add(label5);
+            Controls.Add(label4);
+            ForeColor = SystemColors.ControlLightLight;
+            Name = "Form_Login";
+            Text = "Form1";
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Panel panel1;
+        private Button btn_Login;
+        private TextBox txt_usuario;
+        private Label label1;
+        private TextBox txt_senha;
+        private Label lbl_senha;
+        private Label lbl_usuario;
+        private Label label5;
+        private Label label4;
+        private LinkLabel linklbl_recuperaSenha;
+        private LinkLabel linklbl_cadastro;
+    }
+}
---
+++ ScreenLogin/Form_Login.cs
@@ -0,0 +1,15 @@
+namespace ScreenLogin
+{
+    public partial class Form_Login : Form
+    {
+        public Form_Login()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_Login_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
