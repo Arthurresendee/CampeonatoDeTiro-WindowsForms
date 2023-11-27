af689e73b87f89404147ac761e3ec9de2c2aef74
ArthurWork <Arthurresende2004@gmail.com>
2023-11-26 21:14:12 -0300

    Criando banco de dados através de Migration.

---
+++ ScreenLogin/Data/LoginContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScreenLogin.Data
+{
+    public class LoginContext : DbContext
+    {
+        public DbSet<Usuario> Usuarios { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            // Configure a string de conexão aqui
+            optionsBuilder.UseSqlServer("Data Source=DESKTOP-AJ8AD1P;Initial Catalog=LoginWF;Persist Security Info=True;User ID=sa;Password=root;Connect Timeout=180;Encrypt=False");
+        }
+    }
+}
